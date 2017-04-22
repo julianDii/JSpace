9258d6623e159d079e67f6d94c74f8d837b43af9
julian <julian.dobrot@upday.com>
2017-04-22 10:13:06 +0200

    new line, formatting

--- Jspace/src/app/app.module.ts
+++ Jspace/src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AceEditorComponent } from 'ng2-ace-editor';
-import { AceComponent } from './ace.component';
-import { AppComponent } from './app.component';
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+import { AceEditorComponent }   from 'ng2-ace-editor';
+import { AceComponent }         from './ace.component';
+import { AppComponent }         from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AceEditorComponent,
     AceComponent
-    
   ],
   imports: [
     BrowserModule,
