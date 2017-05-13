25d3eeff72b68fdc1d9d8acaab1e8cc7267630ab
julian <julian.dobrot@upday.com>
2017-05-14 00:02:45 +0200

    updated be node for tokenize requests to send error json for sended code with leading numbers

--- Server/server.js
+++ Server/server.js
@@ -4,8 +4,19 @@ var esprima = require('esprima');
 
 // endpoint for code analyse requests
 app.get('/api/user/tok/:code', function(req, res) {
-    res.json(esprima.tokenize(req.params.code));
-});
+    if(isNaN(req.params.code.charAt(0))){
+        res.json(esprima.tokenize(req.params.code));
+    } else {
+        console.log("error_leadingnumber")
+        res.json(
+            [
+                {
+                    "type": "error",
+                    "value": "Leading numbers are not allowed"
+                }
+            ])
+        } 
+    });
 
 // endpoint for parse user code
 app.get('/api/user/parse/:code', function(req,res) {
