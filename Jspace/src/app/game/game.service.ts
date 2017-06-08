80fccaa328e2680664a87cef5a3f4bb0686e6f05
Inga Schwarze <s59317@beuth-hochschule.de>
2017-06-08 21:04:30 +0200

    - validation for taskElementToArray

--- Jspace/src/app/game/game.service.ts
+++ Jspace/src/app/game/game.service.ts
@@ -45,7 +45,7 @@ export class GameService {
       this.currentTaskNumber = player['task']
     } else {
       console.log("First game...")
-      this.currentTaskNumber = 0;
+      this.currentTaskNumber = 5;
     }
     this.currentTask = this.tasksService.getTask(this.currentTaskNumber);
     console.log('current task', this.currentTask);
