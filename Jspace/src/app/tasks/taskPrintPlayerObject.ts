3b208dc75f80b0be4ead8dd8fb38c6affac53d1f
julian <julian.dobrot@upday.com>
2017-06-08 22:23:58 +0200

    added dummy text

--- Jspace/src/app/tasks/taskPrintPlayerObject.ts
+++ Jspace/src/app/tasks/taskPrintPlayerObject.ts
@@ -9,22 +9,27 @@ export class TaskPrintPlayerObject extends Task {
     constructor() {
         super(
             3,
-            "Objects are variables. But objects can contain many values.Something like:" + "\n" + 
+            "Objects are variables. But objects can contain many values.Something like:" + "\n" +
             "var spacecraft = {model:“Columbia“, weight:“3600kg“};" + "\n" +
-            "The easiest way to print an Object is:" + "\n"+ 
-            "console.log(JSON.stringify(Object));" + "\n"+ 
+            "The easiest way to print an Object is:" + "\n" +
+            "console.log(JSON.stringify(Object));" + "\n" +
             "Your Object, is stored within the Variable user.",
 
-            "",
-            "",
-            "",
-            "",
-            [""]
+            "Try to print your user object to the console",
+
+            "Nice You got it!",
+
+            "Ups! Something went wrong",
+
+            "user",
+
+            ["Error1"]
         );
     }
 
     testTask(json: JSON) {
 
+        // True has to be replaced with validation!
         if (true) {
             let player = JSON.parse(this.localStorageService.readLocalStorage('player'));
             delete player.task;
