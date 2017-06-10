d600636e122f8b6f2255328fc7dce4af13046281
Julian <julian.dobrot@upday.com>
2017-06-10 09:45:47 +0100

    fixed a bug where task number was not iterated in task, implemented print alien object text and local storage (#31)

--- Jspace/src/app/tasks/taskPrintAlienObject.ts
+++ Jspace/src/app/tasks/taskPrintAlienObject.ts
@@ -9,12 +9,33 @@ export class TaskPrintAlienObject extends Task {
     constructor() {
         super(
             6,
-            "", "", "", "", "",
-            [""]
+            "Print out the Alien object.",
+
+            "The friendly alien offers us to look into his bag!" + "\n" +
+            "We can make use of the stringify-function of JSON again." + "\n" +
+            "It is quite easy, you already used this function recently." + "\n" +
+            "Use it again to see what the alien-object has to offer.",
+
+            "Wohooo! The alien has some useful stuff that we can get!",
+            "Did you really used the command like before and with alien?",
+            "Great this is the alien bacckpack: ",
+            ["Wah! It didn’t work! Put the right object name into the brackets? Always check twice!"]
         );
     }
 
     testTask(json: JSON) {
+        if (true) {
+            let player = JSON.parse(this.localStorageService.readLocalStorage('player'));
+            player.task = this.getTaskId() + 1;
+            this.localStorageService.saveToLocalStorage('player', player);
+
+            let alien = {backpack:[]};
+            alien.backpack.push('Super-aluminium');
+            this.localStorageService.saveToLocalStorage('alien', alien);
+            let newMessage = this.getMessageCorrect() + "\n" + JSON.stringify(alien.backpack);
+            this.setMessageCorrect(newMessage);
+        }
+
         return true;
     }
 }
--- Jspace/src/app/tasks/taskPrintPlayerObject.ts
+++ Jspace/src/app/tasks/taskPrintPlayerObject.ts
@@ -33,6 +33,8 @@ export class TaskPrintPlayerObject extends Task {
         // True has to be replaced with validation!
         if (true) {
             let player = JSON.parse(this.localStorageService.readLocalStorage('player'));
+            player.task = this.getTaskId() + 1;
+            this.localStorageService.saveToLocalStorage('player',player);
             delete player.task;
             let newMessage = this.getMessageCorrect() + JSON.stringify(player);
             this.setMessageCorrect(newMessage);
