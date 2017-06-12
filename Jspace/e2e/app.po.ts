92b0edb74b2247bdece156ff537e7c8ecc43dd31
julian <julian.dobrot@upday.com>
2017-06-12 17:47:17 +0200

    inplemented e2e test suite, test cases for first three tasks

--- Jspace/e2e/app.po.ts
+++ Jspace/e2e/app.po.ts
@@ -1,8 +1,17 @@
 import { browser, element, by } from 'protractor';
 
 export class JspacePage {
+
   navigateTo() {
     return browser.get('/');
+  }
+
+  getOutputText() {
+    return element(by.id('ace-output')).getText();
+
+  }
+  setInputText(name) {
+    return element.all(by.className('ace_text-input')).last().sendKeys(name);
   }
 
 }
