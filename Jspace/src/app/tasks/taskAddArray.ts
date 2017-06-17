d1f10c4b6c643a92111fe7f22713ddc8406b69d1
s58856 <s58856@beuth-hochschule.de>
2017-06-17 13:10:06 +0200

    bug fix in error messages

--- Jspace/src/app/tasks/taskAddArray.ts
+++ Jspace/src/app/tasks/taskAddArray.ts
@@ -1,5 +1,5 @@
 import { Task } from './task';
-import { checkSemicolon, stringEqualsString } from '../test-code/helpers';
+import {checkEqualSign, checkSemicolon, stringEqualsString} from '../test-code/helpers';
 import { LocalStorageService } from '../storage/local.storage-service'
 
 export class TaskAddArray extends Task {
@@ -24,19 +24,27 @@ export class TaskAddArray extends Task {
 
       "Nice! Now we have an empty backpack to store our stuff in.",
 
-      "Error! Try it again and check if you really used the right keywords." + "\n" + "Maybe the mentor knows more?"
+      "Error! Try it again and check if you really used the right keywords." + "\n" +
+      "Maybe the mentor knows more?" + "\n" + "\n" +
+      "You should assign an empty array to your backpack."
     );
   }
 
-  private possibleWrongMentorMessages = ["user.backpack misspelled or other identifiers used",
-    "= probably missing",
-    "there should be empty squared brackets",
-    "semicolon probably missing",
-    "There is something in your code which should not be there. It should contain exactly 7 elements."
+  private possibleWrongMentorMessages = ["user.backpack misspelled or other identifiers used", // 0
+    "= probably missing", // 1
+    "opening squared bracket probably missing", // 2
+    "closing squared bracket probably missing or the brackets are not empty", // 3
+    "syntax -> semicolon", // 4
+    "There is something in your code which should not be there. It should contain exactly 7 elements.", // 5
+    "Try again! Something went wrong with your code." + "\n" + // 6
+    "The array must be empty! A little more help?" + "\n" +
+    "The input should look like this: " + "\n" + "object.element = []; (you need to replace object and element for sure)",
   ];
 
   testTask(json: JSON) {
     console.log('tokenized string:', json);
+    this.setMentorAnswerWrong(this.possibleWrongMentorMessages[6]);
+    console.log(this.getMentorAnswerWrong());
 
     let expectedIdentifier1 = "user";
     let expectedIdentifier2 = "backpack";
@@ -44,40 +52,50 @@ export class TaskAddArray extends Task {
 
     if (Object.keys(json).length < 3) {
       let message = "Whoops! You have forgotten something. The elements you typed in are only " + Object.keys(json).length + "." +
-        "\n" + "Check if you used the right operator and assigned the result to oxygen.";
+      "\n" + "You need exactly 7 elements in your code.";
       this.setMentorAnswerWrong(message);
       return false;
     }
 
-    if (Object.keys(json).length >= 3 &&
-      !(stringEqualsString(json[0].value, "user") &&
-      stringEqualsString(json[1].value, ".") &&
-      stringEqualsString(json[2].value, "backpack"))) {
+    if (!this.checkUserBackpack(json)) {
       this.setMentorAnswerWrong(this.possibleWrongMentorMessages[0]);
       return false;
     }
 
-    if (Object.keys(json).length >= 4 &&
-      !(stringEqualsString(json[3].value, "="))) {
+    if (Object.keys(json).length === 3 &&
+      this.checkUserBackpack(json) ||
+      Object.keys(json).length >= 4 &&
+      !checkEqualSign(json[3].value)) {
       this.setMentorAnswerWrong(this.possibleWrongMentorMessages[1]);
       return false;
     }
 
-    if (Object.keys(json).length >= 6 &&
-      !(stringEqualsString(json[4].value, "[") &&
-      stringEqualsString(json[5].value, "]"))) {
+    if (Object.keys(json).length === 4 &&
+      checkEqualSign(json[3].value) ||
+      Object.keys(json).length >= 5 &&
+      !stringEqualsString(json[4].value, "[")) {
       this.setMentorAnswerWrong(this.possibleWrongMentorMessages[2]);
       return false;
     }
 
-    if (Object.keys(json).length >= 7 &&
-      !(checkSemicolon(json[6].value))) {
+    if (Object.keys(json).length === 5 &&
+      stringEqualsString(json[4].value, "[") ||
+      Object.keys(json).length >= 6 &&
+      !stringEqualsString(json[5].value, "]")) {
       this.setMentorAnswerWrong(this.possibleWrongMentorMessages[3]);
       return false;
     }
 
+    if (Object.keys(json).length === 6 &&
+      stringEqualsString(json[5].value, "]") ||
+      Object.keys(json).length >= 7 &&
+      !checkSemicolon(json[6].value)) {
+      this.setMentorAnswerWrong(this.possibleWrongMentorMessages[4]);
+      return false;
+    }
+
     if (Object.keys(json).length > 7) {
-      this.setMentorAnswerWrong(this.possibleWrongMentorMessages[7]);
+      this.setMentorAnswerWrong(this.possibleWrongMentorMessages[5]);
     } else if (Object.keys(json).length === 7){
       codeCorrect = true;
     }
@@ -93,5 +111,12 @@ export class TaskAddArray extends Task {
     }
 
     return codeCorrect;
+  }
+
+  private checkUserBackpack(json) {
+    return Object.keys(json).length >= 3 &&
+    stringEqualsString(json[0].value, "user") &&
+    stringEqualsString(json[1].value, ".") &&
+    stringEqualsString(json[2].value, "backpack");
   }
 }
