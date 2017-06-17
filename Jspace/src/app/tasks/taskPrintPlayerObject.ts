137207b15775dc87fa13ebf18e869f258e42c9a5
janikacek <janikacek@gmail.com>
2017-06-17 15:04:04 +0200

    [JSpace] new validation based on error differentiation of taskPrintPlayerObject (#58)
    * new validation
    
    * bug fix in messages

--- Jspace/src/app/tasks/taskPrintPlayerObject.ts
+++ Jspace/src/app/tasks/taskPrintPlayerObject.ts
@@ -20,69 +20,116 @@ export class TaskPrintPlayerObject extends Task {
 
             "Fabulous! It worked. Move on to the next task.",
 
-            "Try to print your player object again",
+            "Check if you are using console.log and JSON.stringify correctly and if you are printing the object user.",
 
             "This is the pilot object: " + "\n",
 
-            "Ugh! A terrible error occurred. Check what the mentor is saying!"
+            "Ugh! A terrible error occurred." + "\n" + "\n" +
+            "Check what the mentor is saying and try to print your player object again."
         );
     }
 
+    private possibleWrongMentorMessages = ["no console.log at the beginning or not correctly spelled", // 0
+      "missing opening bracket after console.log", // 1
+      "JSON.stringify missing or not correctly spelled", // 2
+      "missing opening bracket after JSON.stringify", // 3
+      "probably not printing the object user", // 4
+      "user written as string in '', not as object", // 5
+      "closing brackets or semicolon missing at the end", // 6
+      "There is something in your code which should not be there. It should contain exactly 12 elements.", // 7
+      "Check if you are using console.log and JSON.stringify correctly and if you are printing the object user."// 8
+    ];
+
     testTask(json: JSON) {
-        if (Object.keys(json).length === 12) {
-            let consoleStr = json[0].value;
-            let dot1 = json[1].value;
-            let log = json[2].value;
-            let firstBracket = json[3].value;
-            let jsonText = json[4].value;
-            let dot2 = json[5].value;
-            let stringify = json[6].value;
-            let secondBracket = json[7].value;
-            let user = json[8].value;
-            let thirdBracket = json[9].value;
-            let fourthBracket = json[10].value;
-            let semicolon = json[11].value;
+      console.log('tokenized string:', json);
+      this.setMentorAnswerWrong(this.possibleWrongMentorMessages[6]);
 
-            if ((stringEqualsString(consoleStr, "console") &&
-                stringEqualsString(dot1, ".") &&
-                stringEqualsString(log, "log") &&
-                stringEqualsString(firstBracket, "(") &&
-                stringEqualsString(jsonText, "JSON") &&
-                stringEqualsString(dot2, ".") &&
-                stringEqualsString(stringify, "stringify") &&
-                stringEqualsString(secondBracket, "(") &&
-                stringEqualsString(user, "user") &&
-                stringEqualsString(thirdBracket, ")") &&
-                stringEqualsString(fourthBracket, ")") &&
-                checkSemicolon(semicolon))) {
+      let codeCorrect = false;
+      let expectedIdentifier = "user";
 
-                let player = JSON.parse(this.localStorageService.readLocalStorage('player'));
-                player.task = this.getTaskId() + 1;
-                this.localStorageService.saveToLocalStorage('player', player);
-                delete player.task;
-                let newMessage = this.getMessageCorrect() + JSON.stringify(player);
-                this.setMessageCorrect(newMessage);
+      if (Object.keys(json).length < 3) {
+        let message = "Whoops! You have forgotten something. The elements you typed in are only " + Object.keys(json).length + "." +
+          "\n" + "You should use console.log and JSON.stringify.";
+        this.setMentorAnswerWrong(message);
+        return false;
+      }
 
-            }
+      if (!this.checkConsoleLog(json)) {
+        this.setMentorAnswerWrong(this.possibleWrongMentorMessages[0]);
+        return false;
+      }
 
-            return (stringEqualsString(consoleStr, "console") &&
-                stringEqualsString(dot1, ".") &&
-                stringEqualsString(log, "log") &&
-                stringEqualsString(firstBracket, "(") &&
-                stringEqualsString(jsonText, "JSON") &&
-                stringEqualsString(dot2, ".") &&
-                stringEqualsString(stringify, "stringify") &&
-                stringEqualsString(secondBracket, "(") &&
-                stringEqualsString(user, "user") &&
-                stringEqualsString(thirdBracket, ")") &&
-                stringEqualsString(fourthBracket, ")") &&
-                checkSemicolon(semicolon));
+      if (Object.keys(json).length === 3 &&
+        this.checkConsoleLog(json) ||
+        Object.keys(json).length >= 4 &&
+        !stringEqualsString(json[3].value, "(")) {
+        this.setMentorAnswerWrong(this.possibleWrongMentorMessages[1]);
+        return false;
+      }
 
+      if (Object.keys(json).length === 4 &&
+        stringEqualsString(json[3].value, "(") ||
+        !this.checkJSONstringify(json)) {
+        this.setMentorAnswerWrong(this.possibleWrongMentorMessages[2]);
+        return false;
+      }
 
+      if (Object.keys(json).length === 7 &&
+        this.checkJSONstringify(json) ||
+        Object.keys(json).length >= 8 &&
+        !stringEqualsString(json[7].value, "(")) {
+        this.setMentorAnswerWrong(this.possibleWrongMentorMessages[3]);
+        return false;
+      }
 
-        } else {
-            console.log('U might forgot something. The elements you typed in are only ' + Object.keys(json).length)
-            return false;
+      if (Object.keys(json).length === 8 &&
+        stringEqualsString(json[7].value, "(") ||
+        Object.keys(json).length >= 9) {
+        if (stringEqualsString(json[8].value, "'user'")) {
+          this.setMentorAnswerWrong(this.possibleWrongMentorMessages[5]);
+          return false;
         }
+        if (!stringEqualsString(json[8].value, "user")) {
+          this.setMentorAnswerWrong(this.possibleWrongMentorMessages[4]);
+          return false;
+        }
+      }
+
+      if (Object.keys(json).length < 12) {
+        this.setMentorAnswerWrong(this.possibleWrongMentorMessages[6]);
+        return false;
+      } else if (Object.keys(json).length > 12) {
+        this.setMentorAnswerWrong(this.possibleWrongMentorMessages[7]);
+      } else {
+        if (stringEqualsString(json[9].value, ")") && stringEqualsString(json[10].value, ")") &&
+          checkSemicolon(json[11].value)) {
+          codeCorrect = true;
+        }
+      }
+
+      if (codeCorrect) {
+        let player = JSON.parse(this.localStorageService.readLocalStorage('player'));
+        player.task = this.getTaskId() + 1;
+        this.localStorageService.saveToLocalStorage('player', player);
+        delete player.task;
+        let newMessage = this.getMessageCorrect() + JSON.stringify(player);
+        this.setMessageCorrect(newMessage);
+      }
+
+      return codeCorrect;
+    }
+
+    private checkConsoleLog(json) {
+      return Object.keys(json).length >= 3 &&
+        stringEqualsString(json[0].value, "console") &&
+        stringEqualsString(json[1].value, ".") &&
+        stringEqualsString(json[2].value, "log");
+    }
+
+    private checkJSONstringify(json) {
+      return Object.keys(json).length >= 7 &&
+      stringEqualsString(json[4].value, "JSON") &&
+      stringEqualsString(json[5].value, ".") &&
+      stringEqualsString(json[6].value, "stringify");
     }
 }
