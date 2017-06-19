26fa7a3bfdf365653d10fecdd220e2cdc995df1a
julian <julian.dobrot@upday.com>
2017-06-19 22:39:26 +0200

    implemented app get/post for user stats

--- Server/server.js
+++ Server/server.js
@@ -18,7 +18,7 @@ router.route("/highscores")
             } else {
                 response = { "error": false, "message": data };
             }
-            res.json(response);
+            res.send(response);
         });
     })
 
